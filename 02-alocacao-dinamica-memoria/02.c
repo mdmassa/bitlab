ec29a76fc004ece4ca43555029a47c9ca8d50282
mdmassa <djsheepwolfbr@gmail.com>
2026-06-04 23:20:51 -0300

    update:atividade 02 alocação dinamica de memoria

--- 02-alocacao-dinamica-memoria/02.c
+++ 02-alocacao-dinamica-memoria/02.c
@@ -2,7 +2,37 @@
 #include <stdlib.h>
 
 int main() {
-    
+    int *nums = (int *)calloc(2,sizeof(int));
+
+    for(int i=0; i<2; i++){
+        printf("Digite o numero %d: ", i+1);
+        scanf("%d", &nums[i]);
+    }
+
+    int *temp = (int *)realloc(nums,4*sizeof(int));
+
+    if(temp != NULL){
+        nums = temp;
+    } else {
+        printf("Erro ao expandir memória.\n");
+        free(nums);
+        nums = NULL;
+        return 1;
+    }
+
+    for(int i=2; i<4; i++){
+        printf("Digite o numero %d: ", i+1);
+        scanf("%d", &nums[i]);
+    }
+
+    printf("\n");
+
+    for(int i=0; i<4; i++){
+        printf("Array[%d]: %d\n", i, nums[i]);
+    }
+
+    free(nums);
+    nums = NULL;
 
     return 0;
 }
